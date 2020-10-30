2b46e1977d788fd3717ca2b5961b3550f9c19d5a
Allan Jales <71303223+allanjales-aja@users.noreply.github.com>
2020-10-30 12:26:10 -0300

    Início do back-end
    Arquivos gerados em 28-10-20 e 29-10-20

---
+++ conexaomultirae.php
@@ -0,0 +1,10 @@
+<?php
+//Conexão de dados no Banco de Dados do MySQL
+    $banco = new mysqli("127.0.0.1", "root", "", "bd_multirae");
+    if ($banco->connect_errno){
+        die("<p> Encontrei um erro ".$banco->errno);
+    }
+    else{
+        echo "Conectado";
+    }
+?>
